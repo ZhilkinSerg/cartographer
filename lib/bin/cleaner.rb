189591de0fcec09af3ed3449f7ed2f894b5ad1a1
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-09-05 14:13:24 +0200

    Cleaner should remove broken maps

--- lib/bin/cleaner.rb
+++ lib/bin/cleaner.rb
@@ -10,12 +10,21 @@ require 'lib/cartographer/worker'
 class Worker < Cartographer::Worker
   def process_job(data)
 
-    log :debug, "Removing directory #{data[:path]}"
+    log :info, "Removing directory #{data[:path]}"
     begin
       FileUtils.remove_entry_secure(data[:path], true)
     rescue Exception => e
       log :error, e.to_s
       log :debug, e.backtrace.join("\n")
+    end
+
+    map = Map.get(data[:id])
+    if map && map.state != :ready
+      log :info, "Removing broken map #{map.id}"
+      unless map.delete
+        log :error, 'Couldn\'t delete broken map'
+        log :debug, map.errors
+      end
     end
   end
 end
